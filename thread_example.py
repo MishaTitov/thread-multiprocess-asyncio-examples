0435d460ac3f4dc0c67f18286bdd13bcb6eb365e
Михаил Титов <michaeltitov1@gmail.com>
2023-03-24 16:02:35 +0300

    added example 2

--- thread_example.py
+++ thread_example.py
@@ -29,16 +29,84 @@ def get_all_weathers_with_threads():
             break
     print(f"With threading done for {time.time() - start} seconds")
 
-def main():
+def example_1():
     print("[+] Run get_all_weathers")
     get_all_weathers()
     print("[+] Run get_all_weathers_with_threads")
     get_all_weathers_with_threads()
 
-if __name__=="__main__":
-    main()
+#################### example 2 ####################
 
+def foo_20_sec(index: int):
+    print(f"Start {index} thread. Sleep 20 sec")
+    time.sleep(20)
+    print(f"End {index} thread")
+
+def foo_5_sec(index: int):
+    print(f"Start {index} thread. Sleep 5 sec")
+    time.sleep(5)
+    print(f"End {index} thread")
+
+def foo_3_sec(index: int):
+    print(f"Start {index} thread. Sleep 3 sec")
+    time.sleep(3)
+    print(f"End {index} thread")
+
+def example_2_helper(arr: list, i:int, index: int) -> int:
+    if i == 0:
+        arr[i] = Thread(target=foo_20_sec,args=(index,), name=f"Thread-{index}")
+    elif i % 3 == 1:
+        arr[i] = Thread(target=foo_5_sec,args=(index,), name=f"Thread-{index}")
+    else:
+        arr[i] = Thread(target=foo_3_sec,args=(index,), name=f"Thread-{index}")
+    index += 1
+    arr[i].start()
+    return index
+
+def example_2():
+    arr = [None] * 3
+    index = 0
+    for i in range(3):
+        index = example_2_helper(arr,i,index)
+    while index < 6:
+        for i in range(3):
+            if arr[i] and not arr[i].is_alive():
+                index = example_2_helper(arr,i,index)
+            else:
+                print(f"{arr[i]} is still running")
+                time.sleep(1)
+
+
+
+if __name__=="__main__":
+    #example_1()
+    example_2()
+
+#################### Prints example 1 ####################
 #[+] Run get_all_weathers
 #Done for 1.4126338958740234 seconds
 #[+] Run get_all_weathers_with_threads
 #With threading done for 0.2989945411682129 seconds
+
+#################### Prints example 2 ####################
+#Start 0 thread. Sleep 20 sec
+#Start 1 thread. Sleep 5 sec
+#Start 2 thread. Sleep 3 sec
+#<Thread(Thread-0, started 30752)> is still running
+#<Thread(Thread-1, started 33632)> is still running
+#<Thread(Thread-2, started 6408)> is still running
+#<Thread(Thread-0, started 30752)> is still running
+#End 2 thread
+#<Thread(Thread-1, started 33632)> is still running
+#End 1 thread
+#Start 3 thread. Sleep 3 sec
+#<Thread(Thread-0, started 30752)> is still running
+#Start 4 thread. Sleep 5 sec
+#<Thread(Thread-3, started 31668)> is still running
+#<Thread(Thread-0, started 30752)> is still running
+#End 3 thread
+#<Thread(Thread-4, started 6560)> is still running
+#Start 5 thread. Sleep 3 sec
+#End 4 thread
+#End 5 thread
+#End 0 thread
